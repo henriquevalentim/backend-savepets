0d3719b31738a7b55d83dcb4d76e672a27d8ba72
Henrique Valentim Nunes <henriquevalentim16@gmail.com>
2022-03-31 22:14:42 -0300

    adjust config port

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -2,7 +2,7 @@ module.exports = {
   HOST: process.env.HOST_DB,
   USER: process.env.USER_DB,
   PASSWORD: process.env.PASSWORD_DB,
-  port: process.env.PORT_DB,
+  PORT: process.env.PORT_DB,
   DB: "savepets",
   dialect: "mysql",
   pool: {
